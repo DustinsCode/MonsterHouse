6050e980bffef856dd241a591a3033b0b8f3e575
xStaticVoid <dthurston1996@gmail.com>
2017-11-14 18:31:18 -0500

    fixed tabs in House and Monster

--- House.py
+++ House.py
@@ -8,17 +8,21 @@ class House(Observer, Obervable):
 A house object
 """
     #TODO: find a better way to randomly fill with monsters
-    def __init__(self, monsters, game):
-        super().register(game)
-        self.population = random.randint(0,10)
-        self.monsters = []
-        self.people = []
-        for i in range(self.population):
-            monsterIndex = random.randint(0,len(monsters)-1)
-            self.monsters.append(monsters[monsterIndex])
+	def __init__(self, monsters, game):
+		super().register(game)
+		self.population = random.randint(0,10)
+		self.monsters = []
+		self.people = []
+		for i in range(self.population):
+			monsterList = [Zombie(), Werewolf(), Vampire(), Ghoul()]
+			monsterIndex = random.randint(0,len(monsters)-1)
+			self.monsters.append(monsters[monsterIndex])
 
-    def update(self, *args):
-        for monster in args:
-            self.monsters.remove(monster)
-            self.people.append(Person())
-        update_observer(len(args))
+	def update(self, *args):
+		for monster in args:
+			self.monsters.remove(monster)
+			self.people.append(Person())
+		update_observer(len(args))
+
+	def getMonsters(self):
+		return self.monsters
--- Monster.py
+++ Monster.py
@@ -3,31 +3,31 @@ from Observable import Observable
 
 class Monster(Observable):
 
-    def __init__(self, house):
-        super().register(house)
+	def __init__(self, house):
+		super().register(house)
 
 
 class Person():
 
-    def __init__(self):
-        self.type = "Person"
+	def __init__(self):
+	self.type = "Person"
 
 class Zombie():
 
-    def __init__(self):
-        self.type = "Zombie"
+	def __init__(self):
+	self.type = "Zombie"
 
 class Vampire():
 
-    def __ init__(self):
-        self.type = "Vampire"
+	def __ init__(self):
+	self.type = "Vampire"
 
 class Ghoul():
 
-    def __init__(self):
-        self.type = "Ghoul"
+	def __init__(self):
+	self.type = "Ghoul"
 
 class Werewolf():
 
-    def __init__(self):
-        self.type = "Werewolf"
+	def __init__(self):
+	self.type = "Werewolf"
